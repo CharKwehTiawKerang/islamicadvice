c63bd6fab28733701c95be046822766f6aa62722
CharKwehTiawKerang <62492138+CharKwehTiawKerang@users.noreply.github.com>
2022-06-24 14:56:17 +0800

    latest

--- index.js
+++ index.js
@@ -1,87 +1,168 @@
 const PORT = process.env.PORT || 8000
 const express = require('express')
-const axios = require('axios')
-const cheerio = require('cheerio')
-const { response } = require('express')
 
 const app = express()
 
+//array objects
+const quotes = 
+    [
+        //Prophet Muhammad (SAW)
+        {
+            quote: 'Speak a good word or remain silent',
+            quotee: 'Prophet Muhammad (peace be upon him)'
+        },
+        {
+            quote: 'The best among you are those who are best to their wives',
+            quotee: 'Prophet Muhammad (peace be upon him)'
+        },
+        {
+            quote: 'The strong man is not the good wrestler, the strong man is only the one who controls himself when he is angry',
+            quotee: 'Prophet Muhammad (peace be upon him)'
+        },
+        {
+            quote: 'If Allah wants to do good to somebody, He afflicts him with trials',
+            quotee: 'Prophet Muhammad (peace be upon him)'
+        },
+        {
+            quote: 'I guarantee a house in Jannah for one who gives up arguing, even if he is in the right; and I guarantee a home in the middle of Jannah for one who abandons lying even for the sake of fun; and I guarantee a house  in the highest part of Jannah for one who has good manners',
+            quotee: 'Prophet Muhammad (peace be upon him)'
+        },
+        {
+            quote: 'Be in this world like a stranger or one who is passing through',
+            quotee: 'Prophet Muhammad (peace be upon him)'
+        },
+        {
+            quote: 'Give charity without delay, for it stands in the way of calamity',
+            quotee: 'Prophet Muhammad (peace be upon him)'
+        },
+        {
+            quote: 'When you are three persons, then two of you should not whisper excluding the third person, for what would grieve him',
+            quotee: 'Prophet Muhammad (peace be upon him)'
+        },
+        {
+            quote: 'Know that victory comes with patience, relief with affliction, and ease with hardship',
+            quotee: 'Prophet Muhammad (peace be upon him)'
+        },
+        {
+            quote: 'For him who follows a path for seeking knowledge, Allah will ease for him the path to Paradise',
+            quotee: 'Prophet Muhammad (peace be upon him)'
+        },
+        //Four Rashidun Caliph
+        {
+            quote: 'Taking pains to remove the pains of others is the true essence of generosity',
+            quotee: 'Abu Bakr as-Siddiq'
+        },
+        {
+            quote: 'Without knowledge action is useless and knowledge without action is futile',
+            quotee: 'Abu Bakr as-Siddiq'
+        },
+        {
+            quote: 'One of the worst sins is a person taking his sin lightly',
+            quotee: 'Abu Bakr as-Siddiq'
+        },
+        {
+            quote: 'He who avoids complaint invites happiness',
+            quotee: 'Abu Bakr as-Siddiq'
+        },
+        {
+            quote: 'A man who is shy and modest is an amazing character, but a woman who is shy and modest is beyond amazing',
+            quotee: 'Abu Bakr as-Siddiq'
+        },
+        {
+            quote: 'Sometimes the people with the worst past, create the best future',
+            quotee: 'Umar Ibn al-Khattab'
+        },
+        {
+            quote: 'No amount of guilt can change the past, and no amount of worrying can change the future',
+            quotee: 'Umar Ibn al-Khattab'
+        },
+        {
+            quote: 'A man should be like a child with his wife, but if she needs him. He should act like a man',
+            quotee: 'Umar Ibn al-Khattab'
+        },
+        {
+            quote: "If you see that someone has slipped, correct him, pray for him, and don't help shaytan against him (by insulting him)",
+            quotee: 'Umar Ibn al-Khattab'
+        },
+        {
+            quote: 'Sit with those who have sinned and repented, for they have the softest of hearts',
+            quotee: 'Umar Ibn al-Khattab'
+        },
+        // {
+        //     quote: 'To be alone means you avoid bad company. But to have a true friend is better than being alone',
+        //     quotee: 'Umar Ibn al-Khattab'
+        // },
+        {
+            quote: 'Worrying about the dunya is a darkness in the heart, while worrying about akhirah is a light in the heart',
+            quotee: 'Uthman Ibn Affan'
+        },
+        {
+            quote: 'Acquire wisdom from the story of those who have already passed',
+            quotee: 'Uthman Ibn Affan'
+        },
+        {
+            quote: 'Silence is the best treatment for anger',
+            quotee: 'Uthman Ibn Affan'
+        },
+        {
+            quote: 'A backbiter harms three persons, firstly himself, secondly the person whom he is addressing and thirdly the person whom he is backbiting',
+            quotee: 'Uthman Ibn Affan'
+        },
+        {
+            quote: 'Knowledge is better than wealth, knowledge protect you but wealth you have to protect it',
+            quotee: 'Uthman Ibn Affan'
+        },
+        {
+            quote: 'Be like a flower that gives its fragrance even to the hand that crushed it',
+            quotee: 'Hazrat Ali Ibn Abi Talib'
+        },
+        {
+            quote: 'There is no wealth like intelligence, no poverty like ignorance, and no inheritance like good manners',
+            quotee: 'Hazrat Ali Ibn Abi Talib'
+        },
+        {
+            quote: 'Never make a decision in anger and never make a promise in happiness',
+            quotee: 'Hazrat Ali Ibn Abi Talib'
+        },
+        {
+            quote: 'Patience is of 2 types: Enduring what you hate & abstaining from what you love',
+            quotee: 'Hazrat Ali Ibn Abi Talib'
+        },
+        {
+            quote: "Do not take someone's silence as his pride, perhaps he is busy fighting with his self",
+            quotee: 'Hazrat Ali Ibn Abi Talib'
+        }
+    ]
 
-const paragraphs = [
-    {
-        name: 'harizasyraf',
-        address: 'https://harizasyraf.netlify.app/',
-        base: 'https://harizasyraf.netlify.app/'
-    },
-    {
-        name: 'robbyleonardi',
-        address: 'http://www.rleonardi.com/',
-        base: ''
-    },
-    {
-        name: 'jack',
-        address: 'https://jacekjeznach.com/',
-        base: ''
-    }
-]
-
-const wordlink = []
+// function to add id and increment it for every object in array
+function addId(arr) {
+    return arr.map(function(obj, index) {
+        return Object.assign({ id: index }, obj);
+    });
+};
+    
+const quotesID = addId(quotes);
 
 
-paragraphs.forEach(paragraph => {
-    axios.get(paragraph.address)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
+// function to get a random item from an array
+function getRandomQuote(arr) {
+    const randomIndex = Math.floor(Math.random() * arr.length);
+    const item = arr[randomIndex];
 
-            $('a:contains("a")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-
-                wordlink.push({
-                    title,
-                    url: paragraph.base + url,
-                    source: paragraph.name
-                })
-            })
-        }) 
-})
+    return item;
+}
 
 
 app.get('/', (req, res) => {
-    res.json('Welcome to my weather API')
+    res.send("Welcome to Islamic Advice API")
 })
 
 
-app.get('/portfolio', (req, res) => {
-    res.json(wordlink)
-})
+app.get('/advice', (req, res) => {
+    const quote = getRandomQuote(quotesID);
+    const json = JSON.stringify(quote);
 
-
-app.get('/portfolio/:portfolioID', (req, res) => {
-    const portfolioID = req.params.portfolioID
-
-    const paragraphAddress = paragraphs.filter(paragraph => paragraph.name == portfolioID)[0].address
-    const paragraphBase = paragraphs.filter(paragraph => paragraph.name == portfolioID)[0].base
-
-    axios.get(paragraphAddress)
-        .then(response => {
-            const html = response.data
-            const $ = cheerio.load(html)
-            const specificWordlink = []
-
-            $('a:contains("a")', html).each(function () {
-                const title = $(this).text()
-                const url = $(this).attr('href')
-
-                specificWordlink.push({
-                    title,
-                    url: paragraphBase + url,
-                    source: portfolioID
-                })
-            })
-            res.json(specificWordlink)
-        }).catch(err => console.log(err)) 
+    res.send(json)
 })
 
 
